d73e34eb1e374abe18968248b2fac53729efc239
Jiangjunyu <double.chiang@gmail.com>
2016-11-06 12:23:31 +0000

    Raspberry agent running stable vesion

---
+++ ds18b20.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+def read_sensor(name):
+
+	dev_sensor = "/sys/bus/w1/devices/" + name + "/w1_slave"
+	# Open the file that we viewed earlier so that python can see what is in it. Replace the serial number as before. 
+	tfile = open(dev_sensor)
+	# Read all of the text in the file. 
+	text = tfile.read() 
+	# Close the file now that the text has been read. 
+	tfile.close() 
+	# Split the text with new lines (\n) and select the second line. 
+	secondline = text.split("\n")[1] 
+	# Split the line into words, referring to the spaces, and select the 10th word (counting from 0). 
+	temperaturedata = secondline.split(" ")[9] 
+	# The first two characters are "t=", so get rid of those and convert the temperature from a string to a number. 
+	temperature = float(temperaturedata[2:]) 
+	# Put the decimal point in the right place and display it. 
+	temperature = temperature / 1000 
+	print temperature
+	return temperature
--- send.py
+++ send.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import pika
 import json
+import time
+
+from ds18b20 import read_sensor
 
 """ Reading configuration file
 assume a default configuratin, if able to read one, overwrite it.
@@ -10,6 +13,7 @@ assume a default configuratin, if able to read one, overwrite it.
 config = {'target_host': 'localhost'}
 with open('thermal_logging.json', 'r') as f:
     config = json.load(f)
+sensors = config['sensors']
 
 credentials = pika.PlainCredentials('guest', 'guest')
 parameters = pika.ConnectionParameters(config['target_host'],
@@ -20,18 +24,23 @@ connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue='thermal')
 
-data = dict()
-data_tags = dict()
-data_fields = dict()
-data_tags['sensor'] = 'sensor1'
-data_fields['value'] = 24
-data['measurement'] = 'thermal'
-data['tags'] = data_tags
-data['fields'] = data_fields
+while True:
+	data = dict()
+	data['measurement'] = 'thermal'
+	data_tags = dict()
+	data_tags['sensor'] = sensors[0]
+	data_fields = dict()
+	data_fields['value'] = read_sensor(sensors[0])
+	data['tags'] = data_tags
+	data['fields'] = data_fields
 
-message = json.JSONEncoder().encode(data)
-channel.basic_publish(exchange='',
-                      routing_key='thermal',
-                      body=message)
-print("[x], send %s" % message)
+	message = json.JSONEncoder().encode(data)
+	channel.basic_publish(exchange='',
+			      routing_key='thermal',
+			      body=message)
+	print("[x], send %s" % message)
+	time.sleep(60)
+
+
 connection.close()
+
