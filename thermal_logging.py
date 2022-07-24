b6204c115a423b60ae27ea45f20875e7648367a7
Chunyu Chiang <double.chiang@gmail.com>
2022-07-24 10:33:55 -0700

    deploy to heroku

--- thermal_logging.py
+++ thermal_logging.py
@@ -25,8 +25,9 @@ class ThermalLogging:
             'project_id' : '88fd3306-59ed-44f4-9134-06d1dd43878c',
             'client_id' : '740269163638-jpthukkg54pab72dadun2q912f18d2d8.apps.googleusercontent.com',
             'client_secret' : '2_I685cVfbMVhJX25fcWNGJE',
-            'refresh_token' : '1//0eQkelEO8pRZsCgYIARAAGA4SNwF-L9IrzNE76R-t3pn3eLy14UBY-85spny7h8GVEqf3E1idE7lF6sOMmWiTKNlYIt6F3sWK9zc'
+            'refresh_token' : '1//068GkykWUrLIsCgYIARAAGAYSNwF-L9IrflJWrXoj2I7bhE2EI_xeAP-iwXe4rlMo_Dqk1Wn9_OMW2ufnO28KLwWgYVvzG5QVkhc'
         }
+
         n = nest.NestAgent(**credentials)
         if n.get_new_access_token():
             # use the new access token to get the devie list
@@ -37,12 +38,13 @@ class ThermalLogging:
                 r.save()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARN)
     t = ThermalLogging()
-    while True:
-        t.startCollectTemp()
-        # sleep 10 minutes.
-        time.sleep(60*10)
+    t.startCollectTemp()
+    # while True:
+    #     t.startCollectTemp()
+    #     # sleep 10 minutes.
+    #     time.sleep(60*10)
 
     
 
